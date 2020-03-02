2a6d86aa729535c299ab0f3d8487e7aef5b9566a
Rowan <rowan.copeland@gmail.com>
2020-03-02 15:21:53 +0000

    added enviroment variables to save Mongo password privately

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 import os
-from flask import Flask 
-
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env 
 
 app = Flask(__name__)
 
+app.config["MONGODB_NAME"] = os.environ.get('MONGODB_NAME')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return'test this'
+@app.route('/view_meetings')
+def view_meetings():
+    return render_template("view_meetings.html", meetings=mongo.db.meetings.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
         debug=True)
-
